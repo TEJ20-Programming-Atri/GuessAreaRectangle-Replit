7bdee4c840ab687923fce2080f326126792f182b
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2023-12-21 16:34:19 +0000

    Fixed

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ length = valid_input("Enter the length of the rectangle: ")
 width = valid_input("Enter the width of the rectangle: ")
 
 #Asks the user for the area
-guessed_area = f"{valid_input('Guess the area of the rectangle: ')}:.2f"
+guessed_area = f"{valid_input('Guess the area of the rectangle: '):.2f}"
 #Calculates Correct Area
-correct_area = f"{length*width}:.2f"
+correct_area = f"{length*width:.2f}"
 
 #Tells them if they got the number right
 if guessed_area == correct_area:
-  print(f"The correct area of the rectangle is {correct_area}")
+  print(f"The correct area of the rectangle is {correct_area}!")
   print(f"You guessed the area correctly! Your answer: {guessed_area}")
 #Tells them if they got it wrong and what the correct answer was
 else:
-  print(f"The correct area of the rectangle is {correct_area}")
+  print(f"The correct area of the rectangle is {correct_area}!")
   print(f"You guessed the area incorrectly! Your answer: {guessed_area}")
